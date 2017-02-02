fb73c4d40f08ce82f3e45900c54ebece089863c9
Kassymov Shyngys <chika.kasymov@gmail.com>
2017-02-02 09:37:03 +0600

    Fixed warning 'Missing submodule' and set .storyboard files to support Xcode 7.x

--- Example/Pods/Target Support Files/UITextField_AutoSuggestion/UITextField_AutoSuggestion-umbrella.h
+++ Example/Pods/Target Support Files/UITextField_AutoSuggestion/UITextField_AutoSuggestion-umbrella.h
@@ -2,6 +2,7 @@
 #import <UIKit/UIKit.h>
 #endif
 
+#import "UITextField+AutoSuggestion.h"
 
 FOUNDATION_EXPORT double UITextField_AutoSuggestionVersionNumber;
 FOUNDATION_EXPORT const unsigned char UITextField_AutoSuggestionVersionString[];
